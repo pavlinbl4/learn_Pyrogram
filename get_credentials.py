e05736d2063bf4d9627eaf21e526eebdf35e8b7d
evgeniy <pavlinbl4@gmail.com>
2024-05-11 22:49:21 +0300

    initial commit

---
+++ get_credentials.py
@@ -0,0 +1,18 @@
+# pip install python-dotenv
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+class Credentials:
+
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.api_id = os.getenv('API_ID')
+        self.api_hash = os.getenv('API_HASH')
+        self.crazypythonbot = os.getenv('CRAZYPYTHONBOT')
+        self.pavlinbl4_bot = os.getenv('PAVLINBL4_BOT')
+        self.admin = os.getenv('ADMIN_ID')
+
+
+if __name__ == '__main__':
+    assert Credentials().admin == '187597961'
